ec363e77efae21de06bc2ff1bf5c47df89f4e7de
kaitlyndeng <kaitlyndeng20@gmail.com>
2023-02-16 10:16:21 -0500

    Commit 6

--- create-task/main.js
+++ create-task/main.js
@@ -4,51 +4,56 @@ const DOMSelectors = {
   input: document.getElementById(".input"),
   box: document.querySelector(".box"),
 };
-// let input = DOMSelectors.input.value;
-const url = "https://api.quotable.io";
 
-async function getData(url) {
-  try {
-    const response = await fetch(url);
-    if (response.status < 200 || response.status > 299) {
-      throw new Error(response);
-    } else {
-      const data = await response.json();
-      console.log(data);
-      console.log("good");
+DOMSelectors.form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log(e);
+  let input = DOMSelectors.input.value;
+  const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${input}`;
+
+  async function getData(url) {
+    try {
+      const response = await fetch(url);
+      if (response.status < 200 || response.status > 299) {
+        throw new Error(response);
+      } else {
+        const data = await response.json();
+        console.log(data);
+        console.log("good");
+      }
+    } catch (error) {
+      console.log(error);
+      console.log("bad");
     }
-  } catch (error) {
-    console.log(error);
-    console.log("bad");
   }
-}
-getData(url);
+  getData(url);
 
-// DOMSelectors.form.addEventListener("submit", function (e) {
-//   e.preventDefault();
-//   console.log(e);
-//   let input = DOMSelectors.input.value;
-//   console.log(input);
-// });
+  async function word(url) {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      data.data
+        .filter((element) => element.word.includes(input))
+        .map((element) => {
+          DOMSelectors.box.insertAdjacentHTML(
+            "beforeend",
+            `<div class="definitions">
+          <h1>${element.meanings}</h1>
+        </div>
+          `
+          );
+        });
+    } catch (error) {
+      console.log(error);
+      console.log("bad");
+    }
+  }
+  word();
+});
 
-// async function word(url, input) {
-//   try {
-//     const response = await fetch(url);
-//     const data = await response.json();
-//     data.data
-//       .filter((element) => element.word.includes(input))
-//       .map((element) => {
-//         DOMSelectors.box.insertAdjacentHTML(
-//           "beforeend",
-//           `<div class="card">
-//           <h1>${element.definition}</h1>
-//         </div>
-//           `
-//         );
-//       });
-//   } catch (error) {
-//     console.log(error);
-//     console.log("bad");
-//   }
+// function listAll() {
+//   data.forEach((data) => {
+//     DOMSelectors.box.insertAdjacentHTML("beforeend", ``);
+//   });
 // }
-// word();
+// listAll();
